2f79d7311b1e2829530735026a295c3ae8d462ab
Riku Kinjo <sansbook@icoud.com>
2024-12-05 13:31:06 +0900

    test_task1

---
+++ test.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xv
+
+ng () {
+       echo ${1}行目の入力に問題あり
+
+}
+
+out=$(echo 3| ./pesky_windows)
+[ "$?" = 0 ] || ng $((LINENO - 1))
+[ "$?" = 0 ] && echo OK
+
+out=$(echo -3| ./pesky_windows)
+[ "$?" = 0 ] || ng $((LINENO - 1))
+[ "$?" = 0 ] && echo OK
+
+out=$(echo 100| ./pesky_windows)
+[ "$?" = 0 ] || ng $((LINENO - 1))
+[ "$?" = 0 ] && echo OK
+
+out=$(echo ABC| ./pesky_windows)
+[ "$?" = 0 ] || ng $((LINENO - 1))
+[ "$?" = 0 ] && echo OK
