9c970d12c65c8f769c591eaffb47629cb8d98f63
Riku Kinjo <sansbook@icoud.com>
2024-12-05 20:27:10 +0900

    test

--- test.bash
+++ test.bash
@@ -4,28 +4,41 @@
 
 ng () {
        echo ${1}行目の入力に問題あり
+       out=1
 }
 
+res=0
+
+out=0
 echo 3| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
 
+out=0
 echo -3| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
 
+out=0
 echo  0| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
 
+out=0
 echo 100| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
 
+out=0
 echo 5.8| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
 
+out=0
 echo ABC| ./pesky_windows
 [ "$?" = 0 ] || ng $((LINENO - 1))
-[ "$?" = 0 ] && echo OK
+[ "$out" = 0 ] && echo OK
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
